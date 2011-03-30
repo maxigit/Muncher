2c359bc1eedfec4de238ca6c3272a230da9e3e6f
Maxime Bourget <mb14@sanger.ac.uk>
2011-03-30 22:07:33 +0100

    add org

--- Muncher.hs
+++ Muncher.hs
@@ -1,18 +1,20 @@
-data Muncher a = Muncher { munch ::  a -> (Maybe a , Muncher a) }
+data Muncher a b = Muncher { munch ::  a -> (Maybe b  , Muncher a b) }
+-- 
 
+build_muncher :: (a -> Either b (Muncher a b))  ->  Muncher a b
 
-group_by_full 0 max g x  =  (Just g , Muncher (group_by_full max max []))
-group_by_full n max g x = (Nothing, Muncher (group_by_full (n-1) max   (g++x)))
+group_by_full 1 max g x  =  (Just g , Muncher (group_by_full max max [x]))
+group_by_full n max g x = (Nothing, Muncher (group_by_full (n-1) max   (g++[x])))
 
 
 group_by n = Muncher ( group_by_full n n [])
 
 
 
-sequouille :: Muncher a -> [a] -> [a] -> [a]
+sequouille :: Muncher a b ->  [a] -> [b] -> [b]
 sequouille m [] r = r
 sequouille m (x:xs) r = case  (munch m x) of 
     (Nothing, m') -> sequouille m' xs  r
-    (Just v, m')  -> sequouille m' xs r++[v]
+    (Just v, m')  -> sequouille m' xs (r++[v])
 --- # TODO:  write with a fold
 
