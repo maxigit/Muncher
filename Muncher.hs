81274013a82af0e990bb457855213470a899ed31
Maxime Bourget <mb14@sanger.ac.uk>
2011-03-30 21:05:30 +0100

    compile

--- Muncher.hs
+++ Muncher.hs
@@ -1,1 +1,6 @@
-data Muncher a = Muncher { munch ::  a -> (Just a , Muncher a) }
+data Muncher a = Muncher { munch ::  a -> (Maybe a , Muncher a) }
+
+
+group_by 0 max g x  =  (Just g , Muncher (group_by max max []))
+group_by n max g x = (Nothing, Muncher (group_by (n-1) max   (g++x)))
+
