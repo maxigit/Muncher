2b1ed2478a5ddffe8b710233318aa0e049c96f53
Maxime Bourget <mb14@sanger.ac.uk>
2011-03-30 21:41:07 +0100

    compile, but we need a different output type

--- Muncher.hs
+++ Muncher.hs
@@ -1,6 +1,18 @@
 data Muncher a = Muncher { munch ::  a -> (Maybe a , Muncher a) }
 
 
-group_by 0 max g x  =  (Just g , Muncher (group_by max max []))
-group_by n max g x = (Nothing, Muncher (group_by (n-1) max   (g++x)))
+group_by_full 0 max g x  =  (Just g , Muncher (group_by_full max max []))
+group_by_full n max g x = (Nothing, Muncher (group_by_full (n-1) max   (g++x)))
+
+
+group_by n = Muncher ( group_by_full n n [])
+
+
+
+sequouille :: Muncher a -> [a] -> [a] -> [a]
+sequouille m [] r = r
+sequouille m (x:xs) r = case  (munch m x) of 
+    (Nothing, m') -> sequouille m' xs  r
+    (Just v, m')  -> sequouille m' xs r++[v]
+--- # TODO:  write with a fold
 
