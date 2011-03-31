4481bb5487c5b78b497730e06c4237b886892866
Maxime Bourget <mb14@sanger.ac.uk>
2011-03-31 16:04:38 +0100

    munch !

--- Muncher.hs
+++ Muncher.hs
@@ -1,20 +1,22 @@
-data Muncher a b = Muncher { munch ::  a -> (Maybe b  , Muncher a b) }
+data Muncher a b = Muncher { munch ::  (Maybe a) -> ( [b]  , Maybe (Muncher a b)) }
 -- 
 
-build_muncher :: (a -> Either b (Muncher a b))  ->  Muncher a b
+--build_muncher :: (a -> Either b (Muncher b b))  ->  Muncher a b
 
-group_by_full 1 max g x  =  (Just g , Muncher (group_by_full max max [x]))
-group_by_full n max g x = (Nothing, Muncher (group_by_full (n-1) max   (g++[x])))
+group_by_ _ group Nothing = (group, Nothing)  -- job finished, muncher die
+group_by_ 0 group _ = (group,  Nothing)
+group_by_ n group (Just x) = ([], Just (Muncher (group_by_ (n-1) (group++[x]))))
+
+group_by n = Muncher ( group_by_ n [])
 
 
-group_by n = Muncher ( group_by_full n n [])
 
-
-
-sequouille :: Muncher a b ->  [a] -> [b] -> [b]
-sequouille m [] r = r
-sequouille m (x:xs) r = case  (munch m x) of 
-    (Nothing, m') -> sequouille m' xs  r
-    (Just v, m')  -> sequouille m' xs (r++[v])
+sequouille_ :: Muncher a b ->  [b] -> [a] -> [b]
+sequouille_ m r [] = r++vs where (vs, _) =  munch m Nothing
+sequouille_ m r (x:xs)  = case  (munch m (Just x)) of 
+    (vs, Nothing) -> r ++ vs
+    (vs, Just  m')  -> sequouille_ m' (r++vs) xs 
 --- # TODO:  write with a fold
+--
+munching muncher = sequouille_ muncher []
 
